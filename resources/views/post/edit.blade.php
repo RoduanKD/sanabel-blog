e361b11dcd80cb22c675ba3b1e2d5bb6e9f04fcb
Your Name <you@example.com>
2021-04-30 14:33:40 +0300

    edit and delete post

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -6,23 +6,33 @@
         @csrf
         <input type="hidden" name="_method" value="PUT">
         <div class="field">
+          @csrf
           <label class="label">Title</label>
           <div class="control">
-            <input class="input" name="title" type="text" placeholder="Post Title" value="{{ $post->title }}">
+            <input class="input @error('title') is-danger @enderror" name="title" type="text" placeholder="Post Title" value="{{ $post->title }}">
+              @error('title')
+                  <p class='help is-danger'>{{$message}}</p>
+              @enderror
           </div>
         </div>
 
         <div class="field">
           <label class="label">Featured Image (URL)</label>
           <div class="control">
-            <input class="input" name="featured_image" type="text" placeholder="http://hi.com/pic.jpg" value="{{ $post->featured_image }}">
+            <input class="input  @error('featured_image')is-danger @enderror" name="featured_image" type="text" placeholder="http://hi.com/pic.jpg" value="{{ $post->featured_image }}">
+              @error('featured_image')
+              <p class='help is-danger'>{{$message}}</p>
+              @enderror
           </div>
         </div>
 
         <div class="field">
           <label class="label">Content</label>
           <div class="control">
-            <textarea class="textarea" name="content" placeholder="Post Content">{{ $post->content }}</textarea>
+            <textarea class="textarea @error('content') is-danger @enderror" name="content" placeholder="Post Content">{{ $post->content }}</textarea>
+            @error('content')
+                <p class='help is-danger'>{{$message}}</p>
+            @enderror
           </div>
         </div>
 
