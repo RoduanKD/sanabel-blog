e5d1eb771368d5eeba10b72daed8504afaa133b7
konouz <konouzkrtoumeh@gmail.com>
2021-05-18 19:38:12 +0300

    resolve slug erorr

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,9 +1,5 @@
 <x-layouts.app>
   Category INdex page
   this is where you will see all categories
-  <ul>
-    @foreach ($category->posts as $post)
-      <li>{{ $post->title }}</li>
-    @endforeach
-  </ul>
+
 </x-layouts.app>
