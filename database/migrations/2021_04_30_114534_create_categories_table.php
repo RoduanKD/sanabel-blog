5ebf65f8c9b696e469be9394bf80ae88a35c4166
konouz <konouzkrtoumeh@gmail.com>
2021-05-18 21:21:00 +0300

    fix slug proplem

--- database/migrations/2021_04_30_114534_create_categories_table.php
+++ database/migrations/2021_04_30_114534_create_categories_table.php
@@ -16,7 +16,6 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('slug');
             $table->string('icon');
             $table->timestamps();
         });
